351862ecbde40f75ba41231a483570765cae8519
AdamCampeau <adamcampeau3@gmail.com>
2022-03-13 19:53:37 -0400

    components

---
+++ src/components/profile/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function profile() {
+  return (
+    <section className="my-5">
+      <h1 id="profile">Profile</h1>
+    </section>
+
+  )
+}
+
+export default About; 
